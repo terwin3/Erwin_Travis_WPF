9220ff030e970b48de846d19c655f1c64ac27178
Travis Erwin <taerwin@fullsail.edu>
2014-10-05 17:31:08 -0500

    String and Boolean Variables
    Used String and Boolean Variables

---
+++ String and Boolean Variables /script.js
@@ -0,0 +1,9 @@
+// Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 18; //Number variable
+
+var isStudent = true; //Boolean Variable
+
+var firstName = "Travis";
+
+var phrase = 'I don\'t know \n you\'ll have to speak with the professor';
