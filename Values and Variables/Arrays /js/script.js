f6c37bf9858ca671d6dfe2e1e36fbdb17978f386
Travis Erwin <taerwin@fullsail.edu>
2014-10-05 20:58:58 -0500

    Arrays
    Finished using Arrays.

--- Values and Variables/Arrays /js/script.js
+++ Values and Variables/Arrays /js/script.js
@@ -2,4 +2,8 @@
 
 var muppetnames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo" ]; //0,1,2,3
 
-console.log(muppetnames[2]);
+muppetnames[1] = "Rizzo"; //change index #1 to Rizzo
+
+var num = 1;
+
+console.log(muppetnames[num]); //lets me see whats in index #1
