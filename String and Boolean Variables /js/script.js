4f1a8e9ce0ce553707b8d8d445af795e583fc254
Travis Erwin <taerwin@fullsail.edu>
2014-10-05 20:55:37 -0500

    Arrays
    Used Arrays

--- String and Boolean Variables /js/script.js
+++
@@ -1,7 +0,0 @@
-// Defining Variables
-
-var age; //declaration
-age = 18; //definition
-//storage container = value
-
-var yearOfbirth = 1996; //declaring and defining on the same line
---
+++ Values and Variables/Arrays /js/script.js
@@ -0,0 +1,5 @@
+// Arrays
+
+var muppetnames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo" ]; //0,1,2,3
+
+console.log(muppetnames[2]);
