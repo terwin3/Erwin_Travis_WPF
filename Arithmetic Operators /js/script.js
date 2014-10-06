99fdec0983ac8ec914c498f7f03c19a5d45b0340
Travis Erwin <taerwin@fullsail.edu>
2014-10-05 21:16:09 -0500

    Arithmetic Operators
    Completed example of Arithmetic Operators

---
+++ Arithmetic Operators /js/script.js
@@ -0,0 +1,9 @@
+//Expressions - Arithmetic Operators
+
+
+//area of the triangle is half of the width times the height
+
+var width = 8;
+var height = 7;
+var area = width * height/2;
+console.log(area);
