fb279142df42929a2fb3eee301f9d1b25d588599
Travis Erwin <taerwin@fullsail.edu>
2014-10-07 15:54:25 -0500

    Personal Expression
    Used prompts, alerts and expressions on personal idea.

---
+++ Expressions Assignment /Personal /js/script.js
@@ -0,0 +1,15 @@
+//Expressions - Personal -
+// Calculating car's fuel efficiency-MPG
+
+var a = 3579; //a is trip a
+var b = 3117; // b is trip b
+var gal = 9.125; // gal is how many gallons it takes to fill car up
+var mpg = (a - b)/gal;
+console.log(mpg);
+prompt("Do you have a truck or a small car?");
+
+//Array
+
+//trips are trip a and b
+var trips = ["Nashville", "Home"];
+console.log(trips);
---
+++ Expressions_Personal /js/script.js
@@ -0,0 +1,17 @@
+//Expressions - Personal -
+// Calculating car's fuel efficiency-MPG
+alert("We\'re going to calculate gas mileage from Nashville to Home.");
+
+var a = 3579; // a is trip a
+var b = 3177; // b is trip b
+var gal = 9.125; // gal is how many gallons it takes to fill car up
+var mpg = (a - b)/gal;
+console.log(mpg);
+
+prompt("Do you drive home or to Nashville more often?:");
+alert("Driving to your home to Nashville is around 44 MPG");
+//Arrays
+
+//Trips are trip a and b
+var trips = ["Nashville", "Home"];
+console.log(trips[1]);
