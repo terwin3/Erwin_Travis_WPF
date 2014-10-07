649e55d326472fe964e39ae5f0f81ec35a74acd1
Travis Erwin <taerwin@fullsail.edu>
2014-10-07 13:34:41 -0500

    Expressions
    Assignment Operators

---
+++ AssociatingJS/Expressions/Casting/js/script.js
@@ -0,0 +1,10 @@
+//Casting
+
+
+var areaCode = 931;
+var firstPart = 279;
+var secPart = 2453;
+//(931) 279-2453
+
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
+console.log(phoneNo);
---
+++ AssociatingJS/Expressions/js/script.js
@@ -0,0 +1,5 @@
+//Expressions - Age Example
+
+var yearBorn = 1996;
+var age = 2014 - yearBorn;
+console.log(age);
