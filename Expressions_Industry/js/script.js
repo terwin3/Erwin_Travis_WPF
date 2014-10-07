b0e2544f121108e05cde3ede7e7f0e600da94f86
Travis Erwin <taerwin@fullsail.edu>
2014-10-07 18:28:47 -0500

    Expressions_Industry
    Created Variables and alerts.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,4 +1,8 @@
 //Expressions - Industry
 //Current job; how much will my check be after two weeks working 30 hours a week making 7.25 an hour
+alert("Going to calculate how much my check will be after two weeks making 7.25 an hour");
+var hours = 30;
+var weeks = 2
+var pay = 7.25;
 
-var
+alert("This is before taxes being taken out");
