defec1eb4d840e73cbb7aa3eb7a521b56dc4a625
Travis Erwin <taerwin@fullsail.edu>
2014-10-07 18:43:20 -0500

    Expressions_industry
    Calculated result of 30 hours for two weeks

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,8 +1,11 @@
 //Expressions - Industry
 //Current job; how much will my check be after two weeks working 30 hours a week making 7.25 an hour
 alert("Going to calculate how much my check will be after two weeks making 7.25 an hour");
-var hours = 30;
+
+var hoursTwoWeeks = 30; //hoursOne and hoursTwo are for each week
 var weeks = 2
-var pay = 7.25;
+var  hours = hoursTwoWeeks * weeks;
+console.log(hours);
+
 
 alert("This is before taxes being taken out");
