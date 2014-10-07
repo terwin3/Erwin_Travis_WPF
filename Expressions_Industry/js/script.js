35b4c2e863e6637daf6e07ca5998e4a18e1193d8
Travis Erwin <taerwin@fullsail.edu>
2014-10-07 18:51:00 -0500

    Industry Expressions
    Finished the problem that calculates what a check will be after two
    weeks

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,11 +1,24 @@
 //Expressions - Industry
 //Current job; how much will my check be after two weeks working 30 hours a week making 7.25 an hour
-alert("Going to calculate how much my check will be after two weeks making 7.25 an hour");
+alert("Calculate how much a check will be after two weeks making 7.25 an hour.");
 
-var hoursTwoWeeks = 30; //hoursOne and hoursTwo are for each week
-var weeks = 2
+//hoursOne and hoursTwo are for each week
+
+var hoursTwoWeeks = 30;
+var weeks = 2;
 var  hours = hoursTwoWeeks * weeks;
 console.log(hours);
 
+//twoWeeks are two weeks of 30 hours times 2
+
+var twoWeeks = 60;
+var hourlyPay = 7.25;
+
+//hourlyPay is 7.25 an hour
+
+var grossPay = twoWeeks * hourlyPay;
+console.log(grossPay);
+
+//grossPay will be what the check is before taxes
 
 alert("This is before taxes being taken out");
