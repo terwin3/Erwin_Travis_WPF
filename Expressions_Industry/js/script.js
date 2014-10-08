521698a02b9e7414238c6221ddc1e41c17a45035
Travis Erwin <taerwin@fullsail.edu>
2014-10-07 19:54:15 -0500

    Industry Expressions
    Changed some code

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,22 +1,16 @@
 //Travis Erwin - October 7th, 2014.
 //Expressions - Industry
-//Current job; how much will my check be after two weeks working 30 hours a week making 7.25 an hour
+//Current job; how much will a check be getting paid every two weeks
 
 
 alert("Calculate how much a check will be after two weeks making 7.25 an hour.");
-
-
- 
-
-
-
+//twoWeeks is hours you work in a period of two weeks.
 var twoWeeks = prompt("How many hours do you work in two weeks?");
-var hourlyPay = 7.25;
-//hourlyPay is 7.25 an hour
+var hourlyPay = prompt("How much do you make an hour?")
+//hourlyPay is what you make an hour.
 
 var grossPay = twoWeeks * hourlyPay;
 console.log(grossPay);
-
-//grossPay will be what the check is before taxes
+//grossPay will be what the check is before taxes.
 
 alert("This is before taxes being taken out");
