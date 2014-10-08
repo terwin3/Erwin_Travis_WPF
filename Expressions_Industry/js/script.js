f30f762847c7d3296643af313ce7dd981fd888c1
Travis Erwin <taerwin@fullsail.edu>
2014-10-07 19:42:19 -0500

    Industry Expressions
    Added prompts and alerts. Removed lines of code that weren’t needed.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,19 +1,17 @@
+//Travis Erwin - October 7th, 2014.
 //Expressions - Industry
 //Current job; how much will my check be after two weeks working 30 hours a week making 7.25 an hour
+
+
 alert("Calculate how much a check will be after two weeks making 7.25 an hour.");
 
-//hoursOne and hoursTwo are for each week
 
-var hoursTwoWeeks = 30;
-var weeks = 2;
-var  hours = hoursTwoWeeks * weeks;
-console.log(hours);
+ 
 
-//twoWeeks are two weeks of 30 hours times 2
 
-var twoWeeks = 60;
+
+var twoWeeks = prompt("How many hours do you work in two weeks?");
 var hourlyPay = 7.25;
-
 //hourlyPay is 7.25 an hour
 
 var grossPay = twoWeeks * hourlyPay;
