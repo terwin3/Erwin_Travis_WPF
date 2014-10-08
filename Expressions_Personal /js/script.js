8f9c6c30df5d96111f26bf0f2955ffd557cc3e82
Travis Erwin <taerwin@fullsail.edu>
2014-10-07 20:11:18 -0500

    Expressions_Personal
    Added working prompts to be interactive with the user to calculate MPG
    (miles per gallon)

--- Expressions_Personal /js/script.js
+++ Expressions_Personal /js/script.js
@@ -1,11 +1,12 @@
 //Travis Erwin - October 7th, 2014.
 //Expressions - Personal
 // Calculating car's fuel efficiency-MPG
-alert("We\'re going to calculate gas mileage from Nashville to Home.");
+alert("We\'re going to calculate gas mileage from two trips.");
 
-var tripA  = prompt("How many miles is your first trip (trip A)?") // tripNashville is the first trip
-var tripB = prompt("How many miles is the second trip (trip B)?"); // tripHome is the second trip
+var tripA  = prompt("How many miles is your first trip (trip A)?") // tripA defines the first trip
+var tripB = prompt("How many miles is the second trip (trip B)?"); // tripB defines the second trip
 var gal = 9.125; // gal is how many gallons it takes to fill car up
 var mpg = ( tripA - tripB)/gal;
-console.log(mpg);
-
+//console.log(mpg);
+var result = "Your MPG is "  + mpg + " mpg";
+alert(result);
