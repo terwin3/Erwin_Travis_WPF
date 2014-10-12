cd5f354969d4514fb12dc6474b9b21d84cbbdaaf
Travis Erwin <taerwin@fullsail.edu>
2014-10-12 17:45:17 -0500

    Conditionals
    Done conditionals example

---
+++ ConditionalsPractice /js/script.js
@@ -0,0 +1,14 @@
+//Conditional Logic -
+
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+//if the price of the phone is less out budget AND if our paycheck is over 300
+if(iPhonePrice < budget || wonLottery === true){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
+
+}
+
