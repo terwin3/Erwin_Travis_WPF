e6e37a1916727d1362d1bcd457701565a9cde8ab
Travis Erwin <taerwin@fullsail.edu>
2014-10-13 18:43:33 -0500

    Conditions-Personal
    Working on adding three variables into code with else if statement

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,11 +2,19 @@
 
 var payCheck = 338;
 var concertVIP = 400;
-
+var winConcertVIP = free
 //if paycheck is enough for concertVIP print to console "You can go! Have fun!"
-if(payCheck < concertVIP){
-    console.log("You can't go, I'm sorry.");
-}
 
+if(payCheck < concertVIP )
+
+
+
+
+else if(payCheck < concertVIP) {
+    console.log("You can't go");
+
+}else{
+    console.log()
+}
 
 
