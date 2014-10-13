6a9e631bf2efec8db8b56f70db8452ec56af78dc
Travis Erwin <taerwin@fullsail.edu>
2014-10-13 18:33:58 -0500

    Conditionals-Personal
    Added if statement and deleted unneeded code.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,9 +1,12 @@
 /* Created by Travis on 10/12/14 */
 
 var payCheck = 338;
-var carInsurance = 96;
-var phoneBill = 103;
-var concertVIP = 150;
+var concertVIP = 400;
+
+//if paycheck is enough for concertVIP print to console "You can go! Have fun!"
+if(payCheck < concertVIP){
+    console.log("You can't go, I'm sorry.");
+}
 
 
 
