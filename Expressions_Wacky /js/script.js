b29770332e9f92536f762e05f981ba761ca27942
Travis Erwin <taerwin@fullsail.edu>
2014-10-14 15:29:46 -0500

    Conditionals-Wacky
    Added prompts to use with conditionals later on

--- Expressions_Wacky /js/script.js
+++ Expressions_Wacky /js/script.js
@@ -1,13 +1,6 @@
-/**
- * Created by Travis on 10/8/14.
- */
-//Calculating how much a song is for a band to record based on members
-var members = prompt ("We are calculating how much a song is to record based on how many members are in a band not calculating mixing and graphic design. Please enter how many members are in your band"); //Band members in band
-var songCost = 400; //Cost per song
-var membersCost = members * songCost;
-console.log(membersCost);
-var result = "It will cost " +  membersCost;
-alert(result);
 
+var preorderCD = prompt("Deciding how much you can spend on a CD preorder. Enter how much the preorder is that you want.");
+
+var spendPayCheck = prompt("Enter how much you'd be willing to spend from your check");
 
 
