b278b0326f3e92b6bf99d0922028dcc244f693c4
Travis Erwin <taerwin@fullsail.edu>
2014-10-13 19:09:50 -0500

    Conditionals-Personal
    Added third var and else if statement

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,14 +2,18 @@
 
 var payCheck = 338;
 var concertVIP = 400;
+var winConcertVIP = 0;
 //var winConcertVIP = 0;
 //if paycheck is enough for concertVIP print to console "You can go! Have fun!"
 
 if(payCheck < concertVIP){
-
+//You can't go!
     console.log("You can't go.");
-}
 
+}else if(payCheck < winConcertVIP){
+
+    
+}
 
 
 
