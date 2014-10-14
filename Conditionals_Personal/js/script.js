03d3e00cc575c66afef37ba75f24d46fb610a479
Travis Erwin <taerwin@fullsail.edu>
2014-10-13 19:45:40 -0500

    Conditionals-Personal
    Changed variables and changed else if and else statement

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,16 +1,19 @@
 /* Created by Travis on 10/12/14 */
 
-var payCheck = 338;
-var concertVIP = 400;
+var payCheck = 438;
+var concertVIP = 100;
+var winConcertVIP =  0;
 
-//var winConcertVIP = 0;
 //if paycheck is enough for concertVIP print to console "You can go! Have fun!"
 
-if(payCheck > concertVIP){
-//You can't go!
-    console.log("You cant go.");
+if(payCheck < concertVIP){
+//You cant go!
+    console.log("You can't go.");
 
-}else if(
+}else if(payCheck === winConcertVIP){
+    console.log("You can go as long as you win!");
+}else{
+    console.log("Go another time");
 }
 
 
