03266952314d790f3e7dee5b00e248afb82ed716
Travis Erwin <taerwin@fullsail.edu>
2014-10-13 19:17:02 -0500

    Conditionals-Personal
    Deleted third var and else if statement

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,15 +2,16 @@
 
 var payCheck = 338;
 var concertVIP = 400;
-var winConcertVIP = 0;
+
 //var winConcertVIP = 0;
 //if paycheck is enough for concertVIP print to console "You can go! Have fun!"
 
-if(payCheck < concertVIP){
+if(payCheck > concertVIP){
 //You can't go!
-    console.log("You can't go.");
+    console.log("You cant go.");
 
-}else if(payCheck > winConcertVIP){
+}else if(
+}
 
 
 
