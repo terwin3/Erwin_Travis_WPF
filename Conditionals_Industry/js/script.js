49fd9de7e516811f4744cf52d04b8ec5093c4014
Travis Erwin <taerwin@fullsail.edu>
2014-10-15 18:56:31 -0500

    Industry example comments
    added comments to lines of code

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,10 +1,11 @@
 /* Created by Travis on 10/15/14. */
-var timeToWork = 5;
+var timeToWork = 5; //time to work usually
+var trafficWork = 15; //time to work if there's traffic
 
-if(timeToWork < 5){
-    drive "Warp speed";
-}else if (timeToWork < 15){
-    drive = "fast";
+if(timeToWork < trafficWork){
+    console.log("Warp speed");
+}else if (timeToWork > trafficWork){
+   console.log("fast");
 }else {
     drive = "speed limit"
 }
