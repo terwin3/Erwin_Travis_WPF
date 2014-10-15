b2af4b5bb252a4ea161607c63b43bbd79ddcec14
Travis Erwin <taerwin@fullsail.edu>
2014-10-15 18:58:26 -0500

    Prompts
    adding prompts to interact with user more

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,12 +1,12 @@
 /* Created by Travis on 10/15/14. */
-var timeToWork = 5; //time to work usually
+var timeToWork = prompt("How long does it take to get to work?") //time to work usually
 var trafficWork = 15; //time to work if there's traffic
 
 if(timeToWork < trafficWork){
     console.log("Warp speed");
 }else if (timeToWork > trafficWork){
    console.log("fast");
-}else {
+}else{
     drive = "speed limit"
 }
 
