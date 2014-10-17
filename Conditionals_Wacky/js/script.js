937706df2609ba1e83fe6e822f96a1c0ee992030
Travis Erwin <taerwin@fullsail.edu>
2014-10-16 20:39:12 -0500

    Spacing
    Spaced code out to be more organized

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,8 +3,8 @@
  */
     //Going to calculate for a cd preorder and how much is left after/before
 var preorderCD = prompt("Deciding how much you can spend on a CD preorder. Enter how much the preorder is that you want.");
-
 var payCheck = prompt("Enter how much your check is");
+
 
 var cost = payCheck - preorderCD;//calculates how much check will be after CD order
 var result = "You will have " + cost + "remaining from your check";
