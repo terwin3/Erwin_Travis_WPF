a9372e63930a9adc2f6866b88d137ece7bdbb8f9
Travis Erwin <taerwin@fullsail.edu>
2014-10-19 10:53:31 -0500

    Functions
    Creating examples for Functions

---
+++ AssociatingJS/Functions/Arguments and Parameters /js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Travis on 10/19/14.
+ */
+//Functions - Parameters and Arguments
+
+function dogYears (age){ //parameters
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.")
+}
+var age1 = 4;
+dogYears(age1);//arguments
+dogYears(5);
