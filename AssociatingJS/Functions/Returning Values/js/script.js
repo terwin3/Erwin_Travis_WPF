8581cbc6100867ac4cee556ffe94180161e14f02
Travis Erwin <taerwin@fullsail.edu>
2014-10-20 17:28:51 -0500

    Functions
    Creating function examples

---
+++ AssociatingJS/Functions/Returning Values/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Travis on 10/19/14.
+ */
+
+var total = calcArea(30,20);
+
+function calcArea(w,h){
+    var area = (w * h);
+    return area; // function spitting the info out
+
+}
+
+console.log(total);
