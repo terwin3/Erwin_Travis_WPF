47acc04fc38a5e7a157db65c6b0c334c9194c4f7
Travis Erwin <taerwin@fullsail.edu>
2014-10-20 17:34:48 -0500

    Anon functions
    example of anonymous functions

---
+++ AssociatingJS/Functions/Anonymous Functions/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Travis on 10/20/14.
+ */
+//Functions - Anonymous Functions
+
+var calcArea = function(width,height){//defining
+    //code the function runs
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea (20, 30);//invoking
+
+console.log(a)
