c0d32f1acefef30539bd02c3fa87bc87359e1dc1
Travis Erwin <taerwin@fullsail.edu>
2014-10-21 19:44:16 -0500

    Else If
    Creating Else If statement

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,4 +3,12 @@
  */
 
 var weatherWarm = prompt("We are deciding if you should wear shorts or jeans to work tomorrow. What is the high temp tomorrow?");
-var weatherChilly = prompt("What is thw low temp for tomorrow?");//prompts for the user to interact
+var weatherChilly = prompt("What is the low temp for tomorrow?");//prompts for the user to interact
+
+if(weatherChilly > weatherWarm){
+//wear jeans
+    console.log("You should wear jeans tomorrow.");
+
+}else if(weatherChilly === weatherWarm){
+    console.log()
+}
