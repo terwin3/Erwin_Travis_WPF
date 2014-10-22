c2502b361e6437b8b27301ac637ddb5b500f47cc
Travis Erwin <taerwin@fullsail.edu>
2014-10-22 10:19:40 -0500

    Arguments-Parameters
    Created arguments to work with parameters

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,10 +2,12 @@
  * Created by Travis on 10/21/14.
  */
 
-function videoGame(){
-    var money = 338.56;
+function videoGame(money){
+
     var videoGame = money - 60;
     console.log("You will have " + videoGame + " dollars remaining from your money")
 }
+var gameSystem = 300;
+videoGame(338.56);
+videoGame(gameSystem);
 
-videoGame();
