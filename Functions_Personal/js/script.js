7150e87cc3feb01530e24469b7a3d5440297c4b3
Travis Erwin <taerwin@fullsail.edu>
2014-10-21 19:35:44 -0500

    Prompts
    Added prompts for user interaction

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Travis on 10/21/14.
+ */
+
+var weatherWarm = prompt("We are deciding if you should wear shorts or jeans to work tomorrow. What is the high temp tomorrow?");
+var weatherChilly = prompt("What is thw low temp for tomorrow?");//prompts for the user to interact
