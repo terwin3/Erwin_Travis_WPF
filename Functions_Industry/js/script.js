94a733127adf2880f6b081a4df7c8cfcae27b936
Travis Erwin <taerwin@fullsail.edu>
2014-10-21 20:25:42 -0500

    Parameters
    created function paramaters

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Travis on 10/21/14.
+ */
+
+workHours(28,30,32); //hours for three weeks at work
+
+function workHours(week1, week2, week3){
+    var hours = week1 + week2 + week3;
+    console.log("Your total hours are " + hours)
+}
---
+++ js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Travis on 10/21/14.
+ */
+
+var weatherWarm = prompt("We are deciding if you should wear shorts or jeans to work tomorrow. What is the high temp tomorrow?");
+var weatherChilly = prompt("What is the low temp for tomorrow?");//prompts for the user to interact
+
+if(weatherChilly > weatherWarm){
+//wear jeans
+    console.log("You should wear jeans tomorrow.");
+
+}else if(weatherChilly < weatherWarm){
+    //
+    console.log("You should wear shorts tomorrow.");
+
+
+}else{
+    //if weather is cold nor warm
+    console.log("Wear what you think is best.");
+}
