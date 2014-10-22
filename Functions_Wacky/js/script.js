343052facbaf749c007ee961a9cf62e0804f2874
Travis Erwin <taerwin@fullsail.edu>
2014-10-21 20:41:54 -0500

    Created function
    Function is created for wacky directory

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Travis on 10/21/14.
+ */
+
+function videoGame(){
+    var money = 338.56;
+    var videoGame = money - 60;
+    console.log("Video game will take away " + videoGame + " from your check")
+}
+
+videoGame();
