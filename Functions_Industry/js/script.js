c9259ca153977aacc93629c3e11e3a6ab6d48ead
Travis Erwin <taerwin@fullsail.edu>
2014-10-23 00:20:36 -0500

    Deleted code
    Deleted un needed code that was commented out

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,13 +2,13 @@
  * Created by Travis on 10/21/14.
  */
 
-//ar workHours(28,30,32); //hours for three weeks at work
+//hours for three weeks at work
 var workHours = function(week1, week2, week3){//parameters
     var hours = week1 + week2 + week3;
     return hours
-    //console.log("Your total hours are " + hours)
+
 }
 
 var result = workHours(28,30,32);
 
-console.log("Your total hours are " + result)
+console.log("Your total hours are " + result);
