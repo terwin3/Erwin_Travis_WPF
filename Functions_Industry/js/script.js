b917e41883b4365716e6342cc92761b1a42d6518
Travis Erwin <taerwin@fullsail.edu>
2014-10-22 23:31:50 -0500

    Comments
    Started to add comments

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,7 +4,7 @@
 
 workHours(28,30,32); //hours for three weeks at work
 
-function workHours(week1, week2, week3){
+function workHours(week1, week2, week3){//parameters
     var hours = week1 + week2 + week3;
     console.log("Your total hours are " + hours)
 }
